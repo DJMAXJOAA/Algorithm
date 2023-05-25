8bd106a47f4d2bbfc6f156a5d2a86c2284269678
djmaxjoaa <cpsn6237@gmail.com>
2023-05-25 15:53:11 +0900

    초기 커밋.

---
+++ Algorithm/20p q1.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int max4(int a, int b, int c, int d);
+
+int main()
+{
+	int a = 9;
+	int b = 3;
+	int c = 5;
+	int d = 2;
+
+	printf("%d", max4(a,b,c,d));
+	return 0;
+}
+
+int max4(int a, int b, int c, int d)
+{
+	int max = a;
+	if (b > max) max = b;
+	if (c > max) max = c;
+	if (d > max) max = d;
+	return max;
+}
