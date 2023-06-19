45681e54f9ce39567f6a8d1570e2e502063d0cf4
djmaxjoaa <cpsn6237@gmail.com>
2023-06-19 16:22:51 +0900

    asdf

--- Algorithm/체스 퀸8개.cpp
+++ Algorithm/체스 퀸8개.cpp
@@ -4,55 +4,120 @@
 #include <cmath>
 #include <vector>
 #include <stdlib.h>
+#include "StopWatch.h"
 
 using namespace std;
 
-const int line = 4;
+const int line = 10;	// ¸î°ö¸î?
 int ct = 0;
 
-
-void Queen(int n, int now, vector<int> chess[], int min, int mid, int max);
-void Print(int n, vector<int> chess[]);
+void Queen(int n, int save, vector<vector<int>> chess);
+void LeftLine(int n, int position, vector<vector<int>>& chess, int fill_or_not);
+void MidLine(int n, int position, vector<vector<int>>& chess, int fill_or_not);
+void RightLine(int n, int position, vector<vector<int>>& chess, int fill_or_not);
+void Print(vector<vector<int>>chess);
 
 int main()
 {
+	StopWatch timer;
+	timer.Start();
 	vector<vector<int>> chess(line, vector<int>(line, 0));
-	Queen(line - 1, 0, *chess[], 0, 0, 0);
-	cout << ct;
-	return 0;
-}
-
-void Queen(int n, int now, vector<int> chess[], int min, int mid, int max)
-{
-	if (n < 0)
-	{
-		ct++;
-		Print(n, chess);
-		return;
-	}
-	
-	if (now == min || now == mid || now == max)
-		return;
-
-	chess[n][now] == 1;
 
 	for (int i = 0; i < line; i++)
 	{
-		Queen(n - 1, i, chess, now - 1, now, now + 1);
+		for (int j = 0; j < i; j++)
+		{
+			chess[0][j] = 0;
+		}
+		chess[0][i] = 2;
+		Queen(1, i, chess);
+		cout << i + 1 << "¹øÂ°" << endl;
+	}
+	cout << ct << "°³" << endl;
+	timer.Stop();
+	cout << timer.getElapsedTime() << "ms °É·ÈÀ¾´Ï´Ù";
+	return 0;
+}
+
+void Queen(int n, int save, vector<vector<int>> chess)
+{
+	if (n == line)
+	{
+		ct++;
+		Print(chess);
+		return;
+	}
+	LeftLine(n, save, chess, 1);
+	MidLine(n, save, chess, 1);
+	RightLine(n, save, chess, 1);
+
+	int j;
+
+	for (j = 0; j < line; j++)
+	{
+		if (chess[n][j] == 1)
+		{
+			continue;
+		}
+
+		for (int i = 0; i < j; i++)
+		{
+			chess[n][i] = 1;
+		}
+		chess[n][j] = 2;
+		Queen(n + 1, j, chess);
+	}
+	LeftLine(n, save, chess, 0);
+	MidLine(n, save, chess, 0);
+	RightLine(n, save, chess, 0);
+	return;
+
+}
+
+void LeftLine(int n, int position, vector<vector<int>>& chess, int fill_or_not)
+{
+	int x = 1;
+	for (int i = n; i < line; i++)
+	{
+		if (position - x < 0)
+			break;
+		chess[i][position - x] = fill_or_not;
+		x++;
 	}
 }
 
-void Print(int n, vector<int> chess[])
+void MidLine(int n, int position, vector<vector<int>>& chess, int fill_or_not)
 {
-	for (int i = 0; i < n; i++)
+	for (int i = n; i < line; i++)
 	{
-		for (int j = 0; j < n; j++)
+		chess[i][position] = fill_or_not;
+	}
+}
+
+void RightLine(int n, int position, vector<vector<int>>& chess, int fill_or_not)
+{
+	int x = 1;
+	for (int i = n; i < line; i++)
+	{
+		if (position + x >= line)
+			break;
+		chess[i][position + x] = fill_or_not;
+		x++;
+	}
+}
+
+void Print(vector<vector<int>> chess)
+{
+	for (int i = 0; i < line; i++)
+	{
+		for (int j = 0; j < line; j++)
 		{
-			if (chess[i][j] == 1)
+			if (chess[i][j] == 2)
 				printf("¡á");
 			else
 				printf("¡à");
 		}
 		printf("\n");
 	}
+	printf("\n");
 }
